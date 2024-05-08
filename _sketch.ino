c4f67ca851fa2c7fce1bbc6c9a9ee9eef4870cb7
tomjamfos <152189579+tomjamfos@users.noreply.github.com>
2024-05-08 12:45:35 +0100

    Update _sketch.ino

--- _sketch.ino
+++ _sketch.ino
@@ -1,15 +1,3 @@
-/*
-Arduino sketch for measuring intensity of light (or anything else!) across a number of sensors and outputting MIDI control 
-change messages. Values are read and 'smoothed' with an exponential weighted moving average filter (inspiration from 
-https://makeabilitylab.github.io/physcomp/advancedio/smoothing-input.html) to avoid 'noisy' readings. Before the filter is 
-applied, the range of sensor values are constrained between a lower and upper limit. This is usefull for using the device 
-in enviroments with different levels of ambient light or adjusting for time of day (uncommenting the for loop near the bottom 
-of the sketch & plotting the raw sensor values, can be usfull for judging this).
-
-The smoothed sensor values sent via MIDI control change messages can be used for controlling parameters in a software application 
-such as Max/MSP or PureData. The sketch makes use of the MidiUSB library (https://www.arduino.cc/reference/en/libraries/midiusb/) 
-to acheive this.
-*/
 #include "MIDIUSB.h"
 #include "EWMA.h"
 
